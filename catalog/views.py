67f76d78dec355b922a5482b8167aad971764f81
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-07-23 14:27:46 +0300

    main page creation

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def index(request):
+    num_books = models.Book.objects.all().count()
+    num_instances = models.BookInstance.objects.all().count()
+    num_instances_available = models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors = models.Author.objects.count()
+    num_genre = models.Genre.objects.all().count()
+    num_books_dota = models.Book.objects.filter(title__icontains='dota').count()
+
+    return render(request,
+                  'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors,
+                           'num_genre': num_genre,
+                           'num_books_dota': num_books_dota,
+                           }
+                  )
